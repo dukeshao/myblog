d2746d9c063d8b968d7d659ff3348caf67037c39
shaozhonghua_ext <zhonghua.shao@zznode.com>
2020-07-19 01:53:35 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,35 @@
+module.exports = {
+    title: '被代码耽误的裁缝',
+    // description: '倔强前端在线烧烤',
+    description: '----------',
+    theme: 'reco',
+    themeConfig: {
+        // 博客配置
+        type: 'blog',
+        blogConfig: {
+            category: {
+                location: 2,     // 在导航栏菜单中所占的位置，默认2
+                text: 'Category' // 默认文案 “分类”
+            },
+            tag: {
+                location: 3,     // 在导航栏菜单中所占的位置，默认3
+                text: 'Tag'      // 默认文案 “标签”
+            }
+        },
+        nav: [
+            { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' }
+        ],
+        head: [
+            ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+        ],
+        // 备案
+        record: '粤ICP备19055048号',
+        // 项目开始时间，只填写年份
+        startYear: '2017',
+        // author
+        author: 'duke shao',
+        logo: '/head.png',
+        authorAvatar: '/head.png',
+        sidebar: 'auto'//在所有页面中启用自动生成侧栏
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,16 @@
+---
+home: true
+heroImage: /head.png
+heroText: null
+heroImageStyle: {
+  maxHeight: '200px',
+  display: block,
+  margin: '6rem auto 0.5rem',
+  borderRadius: '50%',
+  boxShadow: '0 5px 18px rgba(0,0,0,0.2)'
+}
+bgImage: '/blog_bg.png'
+bgImageStyle: {
+  height: '350px'
+}
+---
