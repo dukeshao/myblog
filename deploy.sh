cd09e7fe47256fae45e70b1ac0ff3e1ab984454e
dukeshao <736356772@qq.com>
2020-08-06 15:46:20 +0800

    2020-08-06

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,6 @@
 
 # 确保脚本抛出遇到的错误
 set -e
-
-
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
@@ -14,10 +12,20 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io
-git push -f git@dukeshao:dukeshao/dukeshao.github.io.git master
+expect << EOF
+spawn git push -f https://github.com/dukeshao/dukeshao.github.io.git master
+expect "*sername"
+send "dukeshao\n"
+expect "*assword"
+send "wa711858\n"
+EOF
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# 如果发布到 https://<USERNAME>.github.io
+
+# 走 ssh
+# git push -f git@dukeshao:dukeshao/dukeshao.github.io.git master
+
+# 走 https
+# git push -f https://github.com/dukeshao/dukeshao.github.io.git master
 
 cd -
